27f5f970b54ed61e87ec1526af0343db5b69ff1d
michael magz <michael.magner@gmail.com>
2012-01-05 15:47:07 -0500

    first commit-blank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mugshot31::Application.config.secret_token = '0ebaa86a05bf4879d56a2f91b13246896fefc2ec07ddf6c0123330754656157219d5b709218910addc334d416120a9aa7758aefdf2d13fe83ef2165f18ac1216'
